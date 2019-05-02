d006fcc8b594d99330f32d8e3ee329d57e13b5b9
Patrick W <magetron1@gmail.com>
2019-05-02 22:17:53 +0100

    Add some haskell scripts

--- COMP0002-Exam.hs
+++ COMP0002-Exam.hs
@@ -1,9 +1,17 @@
 myMaximum :: [ Int ] -> Int
 myMaximum [ ] = -32767
 myMaximum (x : xs) = if (myMaximum(xs) > x) then myMaximum(xs) 
-											 else x;
+											 else x
 myLength :: [ x ] -> Int
 myLength [ ] = 0
-myLength (x : xs) = 1 + myLength(xs);
+myLength (x : xs) = 1 + myLength(xs)
 
+add :: Int -> Int -> Int
+add x y = x + y
 
+absoluteVal :: Int -> Int
+absoluteVal x   | x >= 0    = x
+                | otherwise = -x
+
+equalTo :: (Eq a) => a -> a -> Bool
+equalTo x y = x == y
