6099b25696a9fb90ae3518a6286f53d164aa8b18
Patrick W <magetron1@gmail.com>
2019-05-03 16:52:53 +0100

    Add couple of more haskell scripts

--- COMP0002-Exam.hs
+++ COMP0002-Exam.hs
@@ -15,3 +15,32 @@ absoluteVal x   | x >= 0    = x
 
 equalTo :: (Eq a) => a -> a -> Bool
 equalTo x y = x == y
+
+signNum :: Int -> Int
+signNum x = if x < 0 then -1 else
+			if x == 0 then 0 else 
+			1
+
+signNumGuard :: Int -> Int
+signNumGuard x	| x < 0		= -1
+				| x == 0	= 0
+				| otherwise = 1
+				
+safeTail :: [ x ] -> [ x ]
+safeTail [ ] = [ ]
+safeTail (x : xs) = xs
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial x = (factorial (x - 1)) * x
+
+qsort :: (Ord a) => [ a ] -> [ a ]
+qsort [ ] = [ ] 
+qsort (x : xs) = qsort [a | a <- xs, a <= x] 
+					++ [ x ] ++ 
+					qsort [b | b <- xs, b > x]
+
+insert :: (Ord a) => a -> [ a ] -> [ a ] 
+insert x [ ] = [x]
+insert x (y : ys) = if (x > y)	then [ y ] ++ (insert x ys)
+								else [ x ] ++ [ y ] ++ ys
